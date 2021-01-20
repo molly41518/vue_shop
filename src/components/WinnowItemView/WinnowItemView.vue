99ef848076182243b256fb83d7117dbb3eb2a0c7
Molly Chen <candy41518@gmail.com>
2021-01-20 12:17:11 +0800

    WinnowView's component design - WinnowItemView

---
+++ src/components/WinnowItemView/WinnowItemView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="winnow-item-view">
+        <h3>{{item.name}}</h3>
+        <h4>{{item.desc}}</h4>
+        <img :src="item.img" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'winnowItemView',
+  props:['item']
+
+}
+</script>
+
+<style>
+.winnow-item-view{
+    width: 6.25rem;
+    height: 6.25rem;
+    background: #f6f6f6;
+    margin: .3125rem .625rem .625rem 0;
+    text-align: center;
+    float: left;
+}
+.winnow-item-view h3{
+    font-size: .8125rem;
+    margin: .4375rem  .6875rem;
+    font-weight: 700;
+}
+.winnow-item-view h4{
+    font-size: .75rem;
+    margin-bottom: .3125rem;
+    font-weight: 600;
+}
+.winnow-item-view img{
+    width: 4.625rem;
+}
+
+</style>
