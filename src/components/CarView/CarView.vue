15f60be76c8e6c969cd0ce5fde026ef033e4307c
Molly Chen <candy41518@gmail.com>
2021-01-20 16:06:26 +0800

    CarView  Development - CartItem

--- src/components/CarView/CarView.vue
+++ src/components/CarView/CarView.vue
@@ -1,26 +1,48 @@
 <template>
-    <div class="car-view">
-      購物車
-    </div>
+  <div class="car-view">
+    <CartItemView
+      v-for="(item, index) in cartslist"
+      :key="index"
+      :cart="item"
+    ></CartItemView>
+  </div>
 </template>
 
 <script>
+import CartItemView from "../CartItemView/CartItemView.vue";
 export default {
-  name: 'carView', 
+  name: "carView",
+  components: {
+    CartItemView,
+  },
   data() {
     return {
-      title:'購物車',
-      navViewShow:true
-    }
+      title: "購物車",
+      navViewShow: true,
+      cartslist: [],
+    };
   },
   created() {
     // 發送標題
-    this.$emit('ontitle',this.title),
-    this.$emit('onNavShow',this.navViewShow)
+    this.$emit("ontitle", this.title),
+      this.$emit("onNavShow", this.navViewShow);
+
+    var that = this;
+    this.$ajax
+      .get("/json/cart.json")
+      .then(function (response) {
+        that.cartslist = response.data;
+        // console.log(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   },
-}
+};
 </script>
 
 <style>
-
+.car-view {
+  padding-top: 2.8125rem;
+}
 </style>
---
+++ src/components/CartItemView/CartItemView.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="cart-item-view">
+    <div class="cart-checkbox">
+      <van-checkbox
+        v-model="checked"
+        checked-color="red"
+        class="checked-item"
+      ></van-checkbox>
+    </div>
+    <div class="cart-img">
+      <img :src="cart.img" alt="" />
+    </div>
+    <div class="cart-detail">
+      <div class="goods-detail">
+        <span>特價</span>
+        <span>{{ cart.name }}</span>
+      </div>
+      <p>${{ cart.price }}</p>
+      <p>X{{ cart.num }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Checkbox } from "vant";
+export default {
+  name: "cartItemView",
+  components: {
+    [Checkbox.name]: Checkbox,
+  },
+  props: ["cart"],
+  data() {
+    return {
+      checked: true,
+    };
+  },
+};
+</script>
+
+<style>
+.cart-item-view {
+  width: 22.1875rem;
+  height: 6.25rem;
+  margin: 0.625rem;
+  border: 0.0625rem solid #bbbbbb;
+  box-shadow: 0 0 0.25rem 0 gray;
+  border-radius: 0.5rem;
+  background: white;
+}
+.cart-item-view .cart-checkbox {
+  width: 1.875rem;
+  height: 100%;
+  float: left;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+}
+.cart-item-view .cart-img {
+  float: left;
+  width: 4.8125rem;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.cart-item-view .cart-img img {
+  width: 100%;
+}
+.cart-item-view .cart-detail {
+  float: left;
+  margin-top: 1.5625rem;
+}
+.cart-item-view .cart-detail .goods-detail {
+  font-size: 0.875rem;
+}
+.cart-item-view .cart-detail .goods-detail span:nth-child(1) {
+  color: red;
+  margin-right: 0.6875rem;
+}
+.cart-item-view .cart-detail p {
+  font-size: 0.75rem;
+}
+.cart-item-view .cart-detail p:nth-of-type(1) {
+  line-height: 2.1875rem;
+}
+</style>
